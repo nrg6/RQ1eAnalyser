3d2d5ef50fed3d7cc61b5fcbe1d1184589350bc2
nrg6 <nrg6472@hotmail.com>
2023-10-24 14:08:41 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace RQ1_Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+    }
+}
+
---
+++ GlobalUsings.cs
@@ -0,0 +1,21 @@
+﻿global using Microsoft.Maui.ApplicationModel;
+global using Plugin.BLE;
+global using Plugin.BLE.Abstractions;
+global using Plugin.BLE.Abstractions.Contracts;
+global using Plugin.BLE.Abstractions.EventArgs;
+global using System.Collections.ObjectModel;
+global using System.Diagnostics;
+global using IAdapter = Plugin.BLE.Abstractions.Contracts.IAdapter;
+
+global using RQ1_Maui.ViewModels;
+global using Microsoft.Extensions.Logging;
+global using RQ1_Maui.Pages;
+global using RQ1_Maui.Services;
+global using RQ1_Maui.Models;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Maui;
+global using RQ1_Maui.UUIDs;
+global using CommunityToolkit.Maui.Core;
+global using CommunityToolkit.Maui.Alerts;
+
